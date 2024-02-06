1834ac20df6432a3e92544563045be148ea00a65
Savio Ferreira <saviofc.dev@gmail.com>
2024-02-05 21:25:48 -0300

    primeiro componente react  e estilos com css module.

--- js-react/vite-project/src/App.jsx
+++ js-react/vite-project/src/App.jsx
@@ -1,8 +1,9 @@
+import { Header } from "./components/Header/Header"
 
 function App() {
 
   return (
-    <h1>Salve salve</h1>
+    <Header />
   )
 }
 
---
+++ js-react/vite-project/src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import styles from './Header.module.css';
+
+export function Header() {
+    return (
+        <>
+            <header className={styles.header} >
+                <strong>Rei dos piratas</strong>
+            </header>
+
+        </>
+    );
+}
